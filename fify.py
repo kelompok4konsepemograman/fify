63acede05219a23e75dc47545a7a4902caf16a00
Army-Maulana <armymaulanaps28@gmail.com>
2018-12-17 13:41:29 +0700

    first commit

---
+++ fify.py
@@ -0,0 +1,13 @@
+button4 = Button(tk,text=" ",font=('Times 26 bold'), heigh = 4, width = 8, command=lambda:checker(button4))
+button4.grid(row=2, column=0,sticky = S+N+E+W)
+button5 = Button(tk,text=" ",font=('Times 26 bold'), heigh = 4, width = 8, command=lambda:checker(button5))
+button5.grid(row=2, column=1,sticky = S+N+E+W)
+button6 = Button(tk,text=" ",font=('Times 26 bold'), heigh = 4, width = 8, command=lambda:checker(button6))
+button6.grid(row=2, column=2,sticky = S+N+E+W)
+button7 = Button(tk,text=" ",font=('Times 26 bold'), heigh = 4, width = 8, command=lambda:checker(button7))
+button7.grid(row=3, column=0,sticky = S+N+E+W)
+button8 = Button(tk,text=" ",font=('Times 26 bold'), heigh = 4, width = 8, command=lambda:checker(button8))
+button8.grid(row=3, column=1,sticky = S+N+E+W)
+button9 = Button(tk,text=" ",font=('Times 26 bold'), heigh = 4, width = 8, command=lambda:checker(button9))
+button9.grid(row=3, column=2,sticky = S+N+E+W)
+tk.mainloop(
